89c2e0ebab4d3d6c1e754b42fb03732a11f1b4eb
DeeDeeAich <DeeDeeAich@users.noreply.github.com>
2020-12-04 17:13:44 -0600

    Update main.py

--- main.py
+++ main.py
@@ -17,23 +17,40 @@ api_key = json.loads(api_key)
 # Creates first window, sets title, sets the size, sets the icon
 root = Tk()
 root.title("Verse Locator")
-root.geometry("600x100")
+root.geometry("680x225")
 root.iconbitmap("icon.ico")
+
+# Header/Title - Uses Day Roman font (https://www.dafont.com/day-roman.font)
+header = ttk.Label(root, text="Verse Locator", font=("Day Roman", 30)).pack()
 
 # Creates the Entry widget verse_input that takes the user-requested verse
 verse_input = ttk.Entry(root, width=30)
 verse_input.pack()
+ttk.Label(text="", borderwidth=3, font=("Arial", 2)).pack()
+
+# Height variable is for increasing height each time a verse is added to the verse history list.
+height = 225
 
 
 def on_click():
     # Grabs the text put in the entry widget verse_input and stores it in a variable called location
     location = verse_input.get()
+    verse_history = [verse_input.get()]
+
+    for item in verse_history:
+        if item == "":
+            continue
+        global height
+        height += 20
+        Label(root, text=item, font=("Day Roman", 10)).pack()
+        root.geometry(f"680x{height}")
+
     # Deletes all the text entered in the entry widget to give visual effect
     verse_input.delete(0, END)
 
     # Creates a new window
     root2 = Toplevel(root)
-    root2.title("Verse")
+    root2.title("Verse Locator")
     root2.iconbitmap("icon.ico")
 
     # Uses the requests library to grab the inputted verse and search for it in the ESV API
@@ -44,17 +61,34 @@ def on_click():
     verse = json.loads(verse)
 
     # Creates a label in the window that uses the key "passages" and the 0th index of "passages" as the text
-    verse_text = Label(root2, text=verse["passages"][0], font=("Cardo", 10))
-    verse_text.pack(fill="both", expand=1)
+    try:
+        verse_text = Label(root2, text=verse["passages"][0], font=("Cardo", 10))
+        verse_text.pack(fill="both", expand=1)
+    except KeyError:
+        error = Label(root2, text="Error! Verse does not exist!", fg="red")
+        error.pack(fill="both", expand=1)
+        root2.geometry("420x90")
+    except IndexError:
+        error = Label(root2, text="Error! Verse does not exist!", fg="red")
+        error.pack(fill="both", expand=1)
+        root2.geometry("420x90")
 
 
+def enter(event):
+    on_click()
+
+
+root.bind("<Return>", enter)
+locate_button = ttk.Button(root, text="Locate", command=on_click)
+locate_button.pack()
+ttk.Label(text="", borderwidth=3, font=("Arial", 2)).pack()
 # Creates label for requesting verses notice
-ttk.Label(root, font=("Arial", 7), text="Unless otherwise indicated, all Scripture quotations are from the ESV® Bible "
-                                        "(The Holy Bible, English Standard Version®), \ncopyright © 2001 by Crossway, " 
-                                        "a publishing ministry of Good News Publishers. Used by permission. All "
-                                        "rights reserved. \nYou may not copy or download more than 500 consecutive "
-                                        "verses of the ESV Bible or more than one half of any book of the ESV "
-                                        "Bible.").pack()
-ttk.Button(root, text="Ok", command=on_click).pack()
-
+ttk.Label(root, font=("Arial", 7),
+                   text="Unless otherwise indicated, all Scripture quotations are from the ESV® Bible "
+                        "(The Holy Bible, English Standard Version®), \ncopyright © 2001 by Crossway, "
+                        "a publishing ministry of Good News Publishers. Used by permission. All "
+                        "rights reserved. \nYou may not copy or download more than 500 consecutive "
+                        "verses of the ESV Bible or more than one half of any book of the ESV "
+                        "Bible.", borderwidth=15).pack()
+ttk.Label(root, text="Verses History: ", font=("Day Roman", 11)).pack()
 root.mainloop()
